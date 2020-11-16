96f6d08b46ab73edc4bca98e2872b03f597c6ffe
Daniel Adjei Boateng <freshdan24@gmail.com>
2020-11-16 14:04:14 +0000

    database connected

--- config.php
+++ config.php
@@ -1,25 +1,12 @@
 <?php
  //Remote Host
-//  return [
-//   'database' => [
-//   'name' => 'qb4rm7jtklubtn2t',
-//   'username' => 'ln8hqwxsvbmutick',
-//   'password' => 'rjj3w9o0vu818a36',
-//   'connection' => 'mysql:host=bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//   'options' => [
-//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-//   ]]
-//  ]; 
-
- //Localhost
  return [
   'database' => [
-  'name' => 'yenswape',
-  'username' => 'root',
-  'password' => '',
-  'connection' => 'mysql:host=localhost',
+  'name' => 'qb4rm7jtklubtn2t',
+  'username' => 'ln8hqwxsvbmutick',
+  'password' => 'rjj3w9o0vu818a36',
+  'connection' => 'mysql:host=bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   'options' => [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  ]
- ] 
-];
+  ]]
+ ];
