a4dc18b621d960edf896c0729f7874f6b083fbb1
Daniel Adjei Boateng <freshdan24@gmail.com>
2020-11-17 01:07:52 +0000

    ad post error fixed 7

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -1022,7 +1022,6 @@ public function Adimages(){
     App::get('database')->query('INSERT INTO images VALUES (id,:user_id,:ad_id,:images
       )', array('user_id' => isLoggedIn(),':ad_id' => $_SESSION['ad_id'],
       ':images'=> basename($file_name)));
-      echo 'Ad id'.$_SESSION['ad_id'].'Image name'.basename($file_name);
   }else{
      echo $img_upload_errors;
   }
